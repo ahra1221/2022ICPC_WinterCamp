613c0d65ad3749b440180071a79723fba1820413
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2022-02-02 15:05:04 +0900

    왜 안풀리지..

--- 2회차_문자열/A_11091 알파벳 전부 쓰기.cpp
+++ 2회차_문자열/A_11091 알파벳 전부 쓰기.cpp
@@ -8,7 +8,8 @@ int main(){
     int n;
     cin >> n;
     string arr[51];
-    int alphabet[27] = {0, };
+    int alphabet[27] = {1, };
+    int flag = 1;
     for(int i=0;i<n;i++){
         string s;
         getline(cin, s);
@@ -18,17 +19,28 @@ int main(){
         string tmp = arr[i];
         for(int j=0;j<tmp.length();j++){
             if(tmp[j] >= 'A' && tmp[j] <= 'Z'){ //대문자
-                alphabet[tmp[j] - 'A'] = 1;
+                alphabet[tmp[j] - 'A'] = 0;
             }
             else if(tmp[j] >= 'a' && tmp[j] <= 'z'){//소문자
-                alphabet[tmp[j] - 'a'] = 1;
+                alphabet[tmp[j] - 'a'] = 0;
             }
             else{
                 continue;
             }
         }
         for(int j=0;j<27;j++){
-            
+            if(tmp[j]){ continue;}
+            else{
+                flag = 0;
+                break;
+            }
+        }
+        if(flag){ cout << "pangram";}
+        else{
+            cout <<"missing ";
+            for(int j=0;j<27;j++){
+                if(!tmp[j]) cout << to_string(tmp[j]+'a');
+            }
         }
     }
 
