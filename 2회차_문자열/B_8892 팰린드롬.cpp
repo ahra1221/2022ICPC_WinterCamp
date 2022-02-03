a0c51122f629e702b3f17bc7e959391d395cc77a
ahra Ko <79412889+ahra1221@users.noreply.github.com>
2022-02-03 17:29:08 +0900

    solve 8892

--- 2회차_문자열/B_8892 팰린드롬.cpp
+++ 2회차_문자열/B_8892 팰린드롬.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 string s[101];
+int n;
 
 int ispal(int x, int y){
     string str = s[x]+s[y];
-    int len = str.size();
+    int len = (int)str.size();
 
     for(int i=0;i<len/2;i++){
         if(str[i] != str[len-i-1]) return 0;
@@ -13,26 +14,30 @@ int ispal(int x, int y){
     return 1;
 }
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int tc;
-    int n;
-    cin >> tc;
-
-    while(tc--){
-        cin >> n; 
-        for(int i=0;i<n;i++) cin >> s[i];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+void func(){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i!=j){
                 if(ispal(i,j)){
                     cout << s[i] << s[j] <<"\n";
-                    break;
+                    return;
                 }
             }
         }
     }
     cout << "0\n";
+}
 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+
+    while(tc--){
+        cin >> n; 
+        for(int i=0;i<n;i++) cin >> s[i];
+        func();
+    }
     return 0;
 }
